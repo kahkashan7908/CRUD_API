41bac636f52322d6eb0a3671af06a9d53ba34c9d
kahkashan7908 <kahkashankhatoon8@gmail.com>
2023-10-10 11:42:04 +0530

    upload restapi

---
+++ restapipro/restapi/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=150)
+
+    def __str__(self):
+        return self.title
